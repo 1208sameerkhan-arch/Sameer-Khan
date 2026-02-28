c7e0c206964e046fde77091c03ec62cf16e7b301
Sameer <Sameer@Sameers-Macbook.local>
2026-02-28 20:05:03 +0530

    adding file

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-019715e0d74f695be"   # Ubuntu 20.04 x86 (us-east-2)
+  instance_type = "t3.micro"
+  key_name      = "Key-Mac"
+
+  tags = {
+    Name = "My-App-Server"
+  }
+}
