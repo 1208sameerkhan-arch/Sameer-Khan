bcddfe89344637c731fdb2775c50bd9840439118
Sameer <Sameer@Sameers-Macbook.local>
2026-02-28 20:34:42 +0530

    adding

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-019715e0d74f695be"   # Ubuntu 20.04 x86 (us-east-2)
+  ami           = "ami-0b6c6ebed2801a5cb"   # Ubuntu 20.04 x86 (us-east-1)
   instance_type = "t3.micro"
-  key_name      = "Key-Mac"
+  key_name      = "gaurav"
 
   tags = {
     Name = "My-App-Server"
