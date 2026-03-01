4a3d70bc1f9e8f746233a470256385c0b39a8ba8
Sameer <Sameer@Sameers-Macbook.local>
2026-03-02 01:50:33 +0530

    adding

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 # VPC
@@ -15,7 +15,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1"
+  availability_zone       = "ap-south-1"
   map_public_ip_on_launch = true
 
   tags = {
